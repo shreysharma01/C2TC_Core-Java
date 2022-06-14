980dc1fc95d4586f5fdd0410de01ebfa47eb94f0
Shrey Sharma <Shrey Sharma@LAPTOP-C1P45D0Q>
2022-06-14 20:05:43 +0530

    first commit

---
+++ VARIABLE EX-01/src/com/codebode/variables/VariablExercise.java
@@ -0,0 +1,24 @@
+package com.codebode.variables;
+
+public class VariablExercise {
+
+	public static void main(String[] args) {
+		String name = "Shrey";
+		System.out.println(name);
+		int myNum = 15;
+		System.out.println(myNum);
+		System.out.println("Hello"+ name);
+		int myInt = 9;
+		double myDouble=myInt;
+		System.out.println(myDouble);
+		System.out.println(myInt);
+		int i = 010;
+		int j = 07;
+		System.out.println(i);
+		System.out.println(j);
+
+	}
+
+}
+
+
