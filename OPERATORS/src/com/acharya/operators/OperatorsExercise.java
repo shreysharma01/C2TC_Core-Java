42633aaf1a3c9b4f327410f1d9b8035bbc104e62
Shrey Sharma <Shrey Sharma@LAPTOP-C1P45D0Q>
2022-06-14 20:08:17 +0530

    first commit

---
+++ OPERATORS/src/com/acharya/operators/OperatorsExercise.java
@@ -0,0 +1,45 @@
+package com.acharya.operators;
+
+public class OperatorsExercise {
+
+	public static void main(String[] args) {
+		//int a=10;
+		//int b=5;
+		//int c=20;
+		//System.out.println(a<b && a<c);
+		//System.out.println(a<b & a<c);
+		//System.out.println(a<b && a++<c);
+		//System.out.println(a);
+		//System.out.println(a<b & a++<c);
+		//System.out.println(a);
+		
+		//System.out.println(a>b||a<c);
+		//System.out.println(a>b|a<c);
+		//System.out.println(a>b||a++<c);
+		//System.out.println(a);
+		//System.out.println(a>b|a++<c);
+		//System.out.println(a);
+		
+		//int a=2;
+		//int b=5;
+		//int min=(a<b)?a:b;
+		//System.out.println(min);
+		
+		//int a=5;
+		//int b=18;
+		//if ((a=3)==b)
+			//System.out.println(++a);
+		//else
+			//System.out.println(++b);
+		
+		int var1=5;
+		int var2=6;
+		if ((var2=1)==var1)
+			System.out.println(var2);
+		else 
+			System.out.println(++var2);
+		
+        
+	}
+
+}
